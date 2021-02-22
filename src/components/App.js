c17f4672cdfe3bf0b1ae947f552164b5d0288ee8
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-02-22 22:23:33 +0700

    Consume API w. Axios

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,24 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term){
-    console.log(term);  
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID lx1OkJ3xe4OWOPvC9cqsOKFe2Qqtvh1W28DVMgziqhY',
+      },
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px'}}>
+      <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   };
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
