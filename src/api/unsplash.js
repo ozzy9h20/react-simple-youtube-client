e792c66e67fd7c8558e3a6f37b006d31d5f7c262
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-02-23 19:43:45 +0700

    Initial Init

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID lx1OkJ3xe4OWOPvC9cqsOKFe2Qqtvh1W28DVMgziqhY',
-  },
-});
