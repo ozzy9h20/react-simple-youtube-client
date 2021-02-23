7cfaaaff8f6507a3b26dd77903e80cd8c5451557
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-02-23 23:21:54 +0700

    Creating VideoItem List

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
   state = {
@@ -23,7 +24,9 @@ class App extends React.Component {
         <SearchBar 
           onFormSubmit={this.onTermSubmit}
         />
-        I have {this.state.videos.length} videos
+        <VideoList 
+          videos={this.state.videos} 
+        />
       </div>
     );
   }
